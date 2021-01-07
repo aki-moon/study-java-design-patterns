71dce8859155d6eaa5b21f39f824ae78887d6acd
aki.m <matsuaki4@gmail.com>
2021-01-07 21:54:27 +0900

    add singleton pattern

---
+++ singleton/Main.java
@@ -0,0 +1,17 @@
+package singleton;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		Singleton singleton1 = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+		if (singleton1 == singleton2) {
+			System.out.println("singleton1とsingleton2は同じインスタンスです");
+		} else {
+			System.out.println("singleton1とsingleton2は違うインスタンスです");
+		}
+		System.out.println("end");
+	}
+
+}
