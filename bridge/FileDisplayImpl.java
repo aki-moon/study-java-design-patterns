5cd20153aa5b02092ee9670fcf862918415d60d2
aki.m <matsuaki4@gmail.com>
2021-01-12 21:26:35 +0900

    練習問題9-2を解いた

---
+++ bridge/FileDisplayImpl.java
@@ -0,0 +1,51 @@
+package bridge;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileDisplayImpl extends DisplayImpl {
+	private String fileName;
+	private BufferedReader bufferedReader;
+	private static final int MAX_LIMIT_SIZE = 4096;
+
+	public FileDisplayImpl(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+
+	@Override
+	public void rawOpen() {
+		try {
+			bufferedReader = new BufferedReader(new FileReader(fileName));
+			bufferedReader.mark(MAX_LIMIT_SIZE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("*********" + fileName + "*********");
+	}
+
+	@Override
+	public void rawPrint() {
+		String line;
+		try {
+			bufferedReader.reset();
+			while ((line = bufferedReader.readLine()) != null) {
+				System.out.println("> " + line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void rawClose() {
+		System.out.println("*********");
+		try {
+			bufferedReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- bridge/Main.java
+++ bridge/Main.java
@@ -7,11 +7,14 @@ public class Main {
 		Display display2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
 		CountDisplay display3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
 		RandomCountDisplay display4 = new RandomCountDisplay(new StringDisplayImpl("Hello, China"));
+		String filePath = System.getProperty("user.dir") + "\\src\\bridge\\";
+		CountDisplay display5 = new CountDisplay(new FileDisplayImpl(filePath + "sample.txt"));
 		display1.display();
 		display2.display();
 		display3.display();
 		display3.multiDisplay(5);
 		display4.randomDisplay(8);
+		display5.multiDisplay(3);
 	}
 
 }
