9d5559927bafcd3adf88baa895f25f91a260ce5d
aki.m <matsuaki4@gmail.com>
2021-01-12 21:34:55 +0900

    練習問題9-3を解いた

---
+++ bridge/IncreaseDisplay.java
@@ -0,0 +1,21 @@
+package bridge;
+
+public class IncreaseDisplay extends CountDisplay{
+	private int step;
+
+	public IncreaseDisplay(DisplayImpl displayImpl, int step) {
+		super(displayImpl);
+		this.step = step;
+	}
+
+	public void increaseDisplay(int level) {
+		int count =0;
+		for (int i = 0; i < level; i++) {
+			multiDisplay(count);
+			count += step;
+		}
+	}
+
+
+
+}
--- bridge/Main.java
+++ bridge/Main.java
@@ -9,12 +9,16 @@ public class Main {
 		RandomCountDisplay display4 = new RandomCountDisplay(new StringDisplayImpl("Hello, China"));
 		String filePath = System.getProperty("user.dir") + "\\src\\bridge\\";
 		CountDisplay display5 = new CountDisplay(new FileDisplayImpl(filePath + "sample.txt"));
+		IncreaseDisplay display6 = new IncreaseDisplay(new CharDisplayImpl('<', '*', '>'), 1);
+		IncreaseDisplay display7 = new IncreaseDisplay(new CharDisplayImpl('|', '#', '-'), 2);
 		display1.display();
 		display2.display();
 		display3.display();
 		display3.multiDisplay(5);
 		display4.randomDisplay(8);
 		display5.multiDisplay(3);
+		display6.increaseDisplay(4);
+		display7.increaseDisplay(6);
 	}
 
 }
