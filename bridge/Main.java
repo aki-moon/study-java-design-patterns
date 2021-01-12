aa8cb2c874a053f01f00f99ce826e0ac7b0d9034
aki.m <matsuaki4@gmail.com>
2021-01-12 21:11:33 +0900

    練習問題9-1を解いた

--- bridge/Main.java
+++ bridge/Main.java
@@ -6,10 +6,12 @@ public class Main {
 		Display display1 = new Display(new StringDisplayImpl("Hello, Japan."));
 		Display display2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
 		CountDisplay display3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+		RandomCountDisplay display4 = new RandomCountDisplay(new StringDisplayImpl("Hello, China"));
 		display1.display();
 		display2.display();
 		display3.display();
 		display3.multiDisplay(5);
+		display4.randomDisplay(8);
 	}
 
 }
