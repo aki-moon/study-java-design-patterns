9799985ebc37a2fd25cc8e902978773a97a48d5a
aki.m <matsuaki4@gmail.com>
2021-01-14 20:05:33 +0900

    add composite pattern

---
+++ composite/Main.java
@@ -0,0 +1,36 @@
+package composite;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+		System.out.println("Making root entries...");
+		Directory rootDirectory = new Directory("root");
+		Directory binDirectory = new Directory("bin");
+		Directory tmpDirectory = new Directory("tmp");
+		Directory usrDirectory = new Directory("usr");
+		rootDirectory.add(binDirectory);
+		rootDirectory.add(tmpDirectory);
+		rootDirectory.add(usrDirectory);
+		binDirectory.add(new File("vi", 10000));
+		binDirectory.add(new File("latex", 20000));
+		rootDirectory.printList();
+
+		System.out.println("");
+		System.out.println("Making user entries");
+		Directory yuki = new Directory("yuki");
+		Directory hanako = new Directory("hanako");
+		Directory tomura = new Directory("tomura");
+		usrDirectory.add(yuki);
+		usrDirectory.add(hanako);
+		usrDirectory.add(tomura);
+		yuki.add(new File("diary.html", 100));
+		yuki.add(new File("Composite.java", 200));
+		hanako.add(new File("memo.txt", 300));
+		tomura.add(new File("game.doc", 400));
+		tomura.add(new File("junk.mail", 500));
+		rootDirectory.printList();
+		} catch (FileTreatmentException e) {
+			e.printStackTrace();
+		}
+	}
+}
