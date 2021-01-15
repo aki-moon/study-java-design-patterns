ac0a46f859ecafc56dfdd3ee23f206918f5bb42e
aki.m <matsuaki4@gmail.com>
2021-01-15 19:06:20 +0900

    add decorator pattern

---
+++ decorator/FullBorder.java
@@ -0,0 +1,38 @@
+package decorator;
+
+public class FullBorder extends Border {
+
+	protected FullBorder(Display display) {
+		super(display);
+	}
+
+	@Override
+	public int getColumuns() {
+		return 1 + display.getColumuns() + 1;
+	}
+
+	@Override
+	public int getRows() {
+		return 1 + display.getRows() + 1;
+	}
+
+	@Override
+	public String getRowText(int row) {
+		if (row == 0) {
+			return "+" + makeLine('-', display.getColumuns()) + "+";
+		} else if (row == display.getRows() + 1) {
+			return "+" + makeLine('-', display.getColumuns()) + "+";
+		} else {
+			return "|" + display.getRowText(row - 1) + "|";
+		}
+	}
+
+	private String makeLine(char c, int count) {
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < count; i++) {
+			buffer.append(c);
+		}
+		return buffer.toString();
+	}
+
+}
---
+++ decorator/StringDisplay.java
@@ -0,0 +1,31 @@
+package decorator;
+
+public class StringDisplay extends Display {
+	private String string;
+
+	public StringDisplay(String string) {
+		super();
+		this.string = string;
+	}
+
+	@Override
+	public int getColumuns() {
+		return string.getBytes().length;
+	}
+
+	@Override
+	public int getRows() {
+		return 1;
+	}
+
+	@Override
+	public String getRowText(int row) {
+		if (row == 0) {
+			return string;
+		} else {
+			return null;
+		}
+
+	}
+
+}
