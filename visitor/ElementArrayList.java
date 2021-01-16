3becb00c2697f3bf47b4cdeb7d3ccbde2cef0e5d
aki.m <matsuaki4@gmail.com>
2021-01-16 11:38:57 +0900

    練習問題13-3を解いた

---
+++ visitor/ElementArrayList.java
@@ -0,0 +1,17 @@
+package visitor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ElementArrayList extends ArrayList implements Element{
+
+	@Override
+	public void accept(Visitor visitor) {
+		Iterator<?> iterator = iterator();
+		while (iterator.hasNext()) {
+			Element element = (Element) iterator.next();
+			element.accept(visitor);
+		}
+	}
+
+}
