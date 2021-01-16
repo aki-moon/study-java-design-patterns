012f2d5dee25e02ae77fa7541a12d61a17877f94
aki.m <matsuaki4@gmail.com>
2021-01-16 10:36:31 +0900

    練習問題12-2を解いた

--- decorator/MultiStringDisplay.java
+++ decorator/MultiStringDisplay.java
@@ -1,5 +1,50 @@
 package decorator;
 
-public class MultiStringDisplay {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MultiStringDisplay extends Display{
+
+	private List<String> body = new ArrayList<String>();
+	private int columns = 0;
+
+	public void add(String message) {
+		body.add(message);
+		updateColumns(message);
+	}
+
+	@Override
+	public int getColumuns() {
+		return columns;
+	}
+
+	@Override
+	public int getRows() {
+		return body.size();
+	}
+
+	@Override
+	public String getRowText(int row) {
+		return body.get(row);
+	}
+
+	private void updateColumns(String message) {
+		if(message.getBytes().length > columns) {
+			columns = message.getBytes().length;
+		}
+		for (int i = 0; i < body.size(); i++) {
+			int fills = columns - body.get(i).getBytes().length;
+			if (fills > 0) {
+				body.set(i, body.get(i) + spaces(fills));
+			}
+		}
+	}
+
+	private String spaces(int count) {
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < count; i++) {
+			buffer.append(' ');
+		}
+		return buffer.toString();
+	}
 }
