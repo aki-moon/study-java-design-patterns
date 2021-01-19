61ad7090f9eaec23d272be92aa7423c90ce67cdf
aki.m <matsuaki4@gmail.com>
2021-01-20 01:37:01 +0900

    add mediator pattern

--- mediator/Colleague.java
+++ mediator/Colleague.java
@@ -1,6 +1,6 @@
 package mediator;
 
 public interface Colleague {
-	public abstract void setMediator();
+	public abstract void setMediator(Mediator mediator);
 	public abstract void setColleagueEnabled(boolean enabled);
 }
