11d56349867db5b898a9212b404968258e4d36e4
aki.m <matsuaki4@gmail.com>
2021-01-22 23:50:37 +0900

    練習問題20-1を解いた

--- flyweight/BigString.java
+++ flyweight/BigString.java
@@ -4,10 +4,29 @@ public class BigString {
 	private BigChar[] bigChars;
 
 	public BigString(String string) {
+		initShared(string);
+	}
+
+	public BigString(String string, boolean shared) {
+		if(shared) {
+			initShared(string);
+		} else {
+			initUnshared(string);
+		}
+	}
+
+	private void initShared(String string) {
 		bigChars = new BigChar[string.length()];
 		BigCharFactory bigCharFactory = BigCharFactory.getInstance();
 		for (int i = 0; i < bigChars.length; i++) {
 			bigChars[i] = bigCharFactory.getBigChar(string.charAt(i));
+		}
+	}
+
+	private void initUnshared(String string) {
+		bigChars = new BigChar[string.length()];
+		for (int i = 0; i < bigChars.length; i++) {
+			bigChars[i] = new BigChar(string.charAt(i));
 		}
 	}
 
