7cb23d4d761b4b9a2c6b586859a0a874fba07b46
aki.m <matsuaki4@gmail.com>
2021-01-22 23:56:55 +0900

    練習問題20-2を解いた

--- flyweight/Main.java
+++ flyweight/Main.java
@@ -1,8 +1,14 @@
 package flyweight;
 
 public class Main {
+	private static BigString[] bigStrings = new BigString[1000];
 
 	public static void main(String[] args) {
+		System.out.println("共有した場合");
+		testAllocation(true);
+		System.out.println("共有しない場合");
+		testAllocation(false);
+
 		if(args.length == 0) {
 			System.out.println("Usage : java Main digits");
 			System.out.println("Example : java Main 1212123");
@@ -10,6 +16,19 @@ public class Main {
 		}
 		BigString bigString = new BigString(args[0]);
 		bigString.print();
+	}
+
+	private static void testAllocation(boolean shared) {
+		for (int i = 0; i < bigStrings.length; i++) {
+			bigStrings[i] = new BigString("1212123", shared);
+		}
+		showMemory();
+	}
+
+	private static void showMemory() {
+		Runtime.getRuntime().gc();
+		long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println("使用メモリ" + used);
 	}
 
 }
